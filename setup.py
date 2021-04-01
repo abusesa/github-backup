fc9cfe3c7ab34dc19628196fe8c1ec82b3e20641
lnxd <48756329+lnxd@users.noreply.github.com>
2021-04-01 21:31:19 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name='github-backup',
     version='1.0.0',
     description='',
-    url='https://github.com/abusesa/github-backup',
-    author='abusesa',
+    url='https://github.com/lnxd/docker-github-backup',
+    author='lnxd',
     install_requires=['requests'],
     scripts=['github-backup.py'],
     zip_safe=True
