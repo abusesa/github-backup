b2105733c5ebff45c6a3873229b4c623495c80a0
llalon <71791017+llalon@users.noreply.github.com>
2021-07-22 16:29:45 -0400

    Adding inserting default backup directory

--- backup.sh
+++ backup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+BACKUP_DIR="/home/docker/backups"
+
 echo "Project: github-backup"
 echo "Author:  lnxd"
 echo "Base:    Alpine 3.9"
@@ -17,12 +19,12 @@ cp /home/docker/github-backup/config/config.json /home/docker/github-backup/conf
 # Update token in config.json match $TOKEN environment variable
 sed -i '/token/c\   \"token\" : \"'${TOKEN}'\",' /home/docker/github-backup/config.json
 
-# Return config.json to persistant volume
-cp /home/docker/github-backup/config.json /home/docker/github-backup/config/config.json
+# Update backup directory to match the containers default
+sed -i '/directory/c\   \"directory\" : \"'${BACKUP_DIR}'\",' /home/docker/github-backup/config.json
 
 # Start backup
 while true
  do python3 github-backup.py /home/docker/github-backup/config/config.json
- chown -R nobody /home/docker/backups
- sleep $SCHEDULE
+ chown -R nobody ${BACKUP_DIR}
+ sleep "$SCHEDULE"
 done
